a51c82b1a31e9866e08fec0493fab27cdb3435fb
Fahed Mbarek <fahedmbarek9@gmail.com>
2023-11-30 10:11:54 +0100

    Initial commit.

---
+++ src/main/java/com/example/spring_boot_security/SecuringWebApplication.java
@@ -0,0 +1,10 @@
+package com.example.spring_boot_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class SecuringWebApplication {
+    public static void main(String[] args) throws Throwable {
+        SpringApplication.run(SecuringWebApplication.class, args);
+    }
+}
